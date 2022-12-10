5ed2026088e519a57574912285315f018977d819
Martin Švorc <svorcmar@gmail.com>
2022-12-10 16:40:57 +0100

    kt: main, readLine -> readln

--- src/6_1.kt
+++ src/6_1.kt
@@ -1,2 +1,4 @@
 // AOC 2022 6-1
-println(readLine()!!.windowed(4).withIndex().first { it.value.toSet().size == 4 }.index + 4)
+fun main() {
+    println(readln().windowed(4).withIndex().first { it.value.toSet().size == 4 }.index + 4)
+}
--- src/6_2.kt
+++ src/6_2.kt
@@ -1,2 +1,4 @@
 // AOC 2022 6-2
-println(readLine()!!.windowed(14).withIndex().first { it.value.toSet().size == 14 }.index + 14)
+fun main() {
+    println(readln().windowed(14).withIndex().first { it.value.toSet().size == 14 }.index + 14)
+}
